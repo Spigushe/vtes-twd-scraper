d4a3c304a074852c18268a027bc1d4c43fd97be7
Spigushe <martin.cuchet@gmail.com>
2026-03-09 19:00:30 +0100

    refactor: remove obsolete test file for tournament YAML processing

--- vtes_scraper/test.py
+++
@@ -1,9 +0,0 @@
-from ruamel.yaml import YAML
-from vtes_scraper.models import Tournament
-from vtes_scraper.output.txt import tournament_to_txt
-
-yaml = YAML()
-with open("twds/2026/02/12822.yaml") as f:
-    data = yaml.load(f)
-    t = Tournament(**data)
-    print(tournament_to_txt(t))
